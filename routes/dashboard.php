633fa948eab2006b9d88be57feaad801fd80c6d8
Moon2340 <farjanamoon20144@gmail.com>
2024-03-05 23:38:30 +0600

    login done

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Voting System- Dashboard</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
+
 <body>
     <button>Back</button>
     <button>Logout</button>
@@ -13,4 +16,5 @@
     <div id=Profile></div>
     <div id=Group></div>
 </body>
+
 </html>
--- routes/register.php
+++ routes/register.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Voting System- Registration</title>
 </head>
+
 <body>
     <h1>Online Voting System</h1>
     <hr>
@@ -15,13 +17,14 @@
         <input type="password" name="password" placeholder="password">
         <input type="password" name="cpassword" placeholder="Confirm password"><br><br>
         <input type="text" name="address" placeholder="Address"><br><br>
-       Upload image: <input type="file" name="photo"><br><br>
-       Select role: <select name="role">
-<option value="1">Voter</option>
-<option value="2">Group</option>
+        Upload image: <input type="file" name="photo"><br><br>
+        Select role: <select name="role">
+            <option value="1">Voter</option>
+            <option value="2">Group</option>
         </select> <br><br>
         <button>Register</button><br><br>
-        Already user? <a href="#">Login Here</a>
+        Already user? <a href="../index.php">Login Here</a>
     </form>
 </body>
+
 </html>
